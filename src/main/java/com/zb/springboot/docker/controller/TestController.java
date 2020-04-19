51f40e7ad97400ffd3828edaedefaccaa70aca85
zhangbing <1060570381@qq.com>
2020-04-19 15:48:03 +0800

    测试

---
+++ src/main/java/com/zb/springboot/docker/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.zb.springboot.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "test")
+@RestController
+public class TestController {
+
+    @GetMapping("hello")
+    public String hello(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+}
---
+++ src/test/java/com/zb/springboot/docker/DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zb.springboot.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
